57aae1c12ac8599fc3eec80a81913bfb4742ad87
Alexander Peschel <alexanderpeschel@me.com>
2016-10-25 13:27:03 +0200

    resize images

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,47 @@
 "use strict";
 
-var gulp = require('gulp'),
-  rename = require('gulp-rename'),
-    sass = require('gulp-sass'),
-    maps = require('gulp-sourcemaps'),
-    del = require('del');
+var  gulp = require('gulp'),
+   rename = require('gulp-rename'),
+     sass = require('gulp-sass'),
+     maps = require('gulp-sourcemaps'),
+      del = require('del'),
+   useref = require('gulp-useref'),
+   gulpif = require('gulp-if'),
+     minifyCss = require('gulp-clean-css'),
+ imagemin = require('gulp-imagemin');
+
+var options = {
+    src: 'src',
+    dist: 'dist'
+};
 
 gulp.task('compileSass', function() {
-  return gulp.src("scss/main.scss")
+  return gulp.src(options.src + "/scss/main.scss")
       .pipe(maps.init())
       .pipe(sass())
       .pipe(maps.write('./'))
-      .pipe(gulp.dest('css'));
+      .pipe(gulp.dest(options.src + '/css'));
 });
 
 gulp.task('watchFiles', function() {
-  gulp.watch('scss/**/*.scss', ['compileSass']);
+  gulp.watch(options.src + '/scss/**/*.scss', ['compileSass']);
+});
+
+gulp.task('jpgs', function() {
+    return gulp.src(options.src + '/img/**/*')
+    .pipe(imagemin({ progressive: true }))
+    .pipe(gulp.dest(options.dist + '/img'));
 });
 
 gulp.task('clean', function() {
   del(['optimization-testing/public', 'css/main.css*']);
+});
+
+gulp.task('html', ['compileSass'], function() {
+  return gulp.src(options.src + '/index.html')
+      .pipe(useref())
+      .pipe(gulpif('*.css', minifyCss()))
+      .pipe(gulp.dest(options.dist));
 });
 
 gulp.task("build", ['compileSass'], function() {
